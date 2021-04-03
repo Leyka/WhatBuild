4471a56624b46784dc88f9161e76dc9c063b53f8
Skander <skander.kc@gmail.com>
2021-04-03 19:59:21 -0400

    Add item set generator

---
+++ WhatBuild.Core/ItemSetGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WhatBuild.Core.Interfaces;
+
+namespace WhatBuild.Core
+{
+    public class ItemSetGenerator
+    {
+        public IBuildSource BuildSource { get; set; }
+        public string LoLInstallPath { get; set; }
+
+        public ItemSetGenerator(IBuildSource source, string lolInstallPath)
+        {
+            BuildSource = source;
+            LoLInstallPath = lolInstallPath;
+        }
+
+        /// <summary>
+        /// Create a Item Set JSON file for all champions under LoL install path 
+        /// </summary>
+        public void GenerateItemSetForAllChampions()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
